ba0fc8a464c2ee47a50fa851793b3e73ca41efcd
jbuan <jerome.buan.06@gmail.com>
2022-10-12 21:07:47 +0200

    autre vision du main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,4 +1,5 @@
 #include "../headers/headers.hpp"
+#include <cstdlib>
 
 bool isNumber(const std::string s)
 {
@@ -7,35 +8,57 @@ bool isNumber(const std::string s)
     return !s.empty() && it == s.end();
 }
 
-std::map<std::string, std::string> parser( int argc, char **argv)
+int	translate_port( char *port )
 {
+// est-ce qu'on rajoute une limite de la securite sur les args donnés ? 
+// on pourrait rajouter le nombre de caracteres, une limite au port
 
-	std::map<std::string, std::string> word;
+	int	i;
 
-	if ( argc != 3 ) {
-		throw errorExcute("Usage: ./ircserv <port> <password> \n");
-	}
-	if (!isNumber(argv[1])) {
+	if (!isNumber(port)) 
 		throw errorExcute("Usage: <port> must be numeric");
-	}
-	word["PORT"] = argv[1];
-	word["SERVER_PWD"] = argv[2];
-	return ( word );
+	i = atoi( port );
+	if ( i < 0 || i > 65535 )
+		throw errorExcute("Usage: <port> can't be 0, negative or more
+		than 65535");
+// peut être faire une fonction qui vérifie si les ports sont utilisés ou pas
+	return ( i );
 }
 
+std::string	password( std::string pass_tmp )
+{
+	std::string pass;
+//	sécu potentielle à rajouter
+	pass = pass_tmp;
+	return ( pass );
+}
 
 int main(int argc, char **argv)
 {
 
-    std::map<std::string, std::string> m;
+	int		port; //je trouve que c'est plus simple sans map
+	std::string 	pass; //moins de nommage, 2 trucs c'est peu pour map
 
-	//Server	server;
+// On verfie qu'on est assez d'arguments, que l'argv1, le port, n'est pas de 
+// lettre, puis on les stocks 
 
 	try {
-        m = parser( argc, argv);
+	if ( argc != 3 ) 
+		throw errorExcute("Usage: ./ircserv <port> <password> \n");
+        port = translate_port(argv[1]);
+        pass = password((std::string)argv[2]);
 	}
 	catch (const std::exception & e) {
 		std::cerr << "exception found: " << e.what() << std::endl;
+		return ( 1 );
 	}
+
+// l'etape d'après est la construction du serveur, m nous servira a définir 
+// le port et le mot de passe
+
+//	server(port, pass);
+
+	std::cout << "Port = " << port << " && Pass = " << pass << std::endl;
+
 	return (0);
 }
